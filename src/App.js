53c97e07252afffdf52503f822ad8914c85878ca
koderoom <koderoom.dev@gmail.com>
2021-01-19 11:51:54 +0530

    working with list.

--- src/App.js
+++ src/App.js
@@ -7,30 +7,18 @@ import "./App.css";
 function App() {
   return (
     <div>
+      <Page1 />
+
       <Page2 />
     </div>
   );
 }
 
-function AboutUs() {
-  return <div>About Us</div>;
-}
-
-/**
- * How will we use variable inside jsx
- * {}
- */
 function Page1() {
   // Logical JS
   const num = 100;
-
-  return <div>Hello {num}</div>;
-}
-
-function Page2() {
-  // Logical JS
-  const num = 100;
   const str = "Hello World";
+  const ref = { id: 1, city: "Mumbai", state: "Maharashtra" };
   const sum = (a, b) => a + b;
 
   return (
@@ -38,6 +26,33 @@ function Page2() {
       <div>Number {num}</div>
       <div>String {str}</div>
       <div>Function :: {sum(1, 1)}</div>
+
+      <div>Ref City Name {ref.city} </div>
+      <div>Ref State name {ref.state}</div>
+    </div>
+  );
+}
+
+function Page2() {
+  const str = "Hello world";
+  const list = ["Delhi", "Kolkata", "Mumbai", "Chennai"];
+
+  const list1 = list.map((item, index) => <h3 key={index}>{item}</h3>);
+
+  return (
+    <div>
+      <h2>Page2 List Option 1 Arrow Function with Return Statemetn</h2>
+      {list.map((item, index) => {
+        return <h3 key={index}>{item} </h3>;
+      })}
+
+      <h2>Page 2 List Option 2 :: Arrrow Function in One LIne</h2>
+      {list.map((item, index) => (
+        <h3 key={index}>{item}</h3>
+      ))}
+
+      <h2>Option 3:: We have extracted the logic at top level </h2>
+      {list1}
     </div>
   );
 }
