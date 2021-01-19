884192455407c41b045799434a6c945c655482e7
koderoom <koderoom.dev@gmail.com>
2021-01-19 12:17:59 +0530

    decision making.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div>
+      <Greeting1 />
+      <Greeting />
       <Page1 />
     </div>
   );
@@ -18,6 +20,38 @@ function Page1() {
       {list.map((item, index) => (
         <h3 key={index}>{item}</h3>
       ))}
+    </div>
+  );
+}
+
+function Greeting() {
+  const isMorning = true;
+
+  return (
+    <div>
+      <div>Message :</div>
+
+      {/* An Example of IF Condition */}
+      {isMorning && <div>Good Morning</div>}
+      {isMorning && (
+        <div>
+          <div>Multiline Good Morning</div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Greeting1() {
+  const checkIfTrue = false;
+
+  return (
+    <div>
+      <div>IF and ELSE using Ternary Operator :</div>
+
+      {/* An Example of IF Condition */}
+      {/* expression ? 'Good Moring' : 'Good Afternoon' */}
+      {checkIfTrue ? <div>Good Morning</div> : <div>Good Afternoon</div>}
     </div>
   );
 }
