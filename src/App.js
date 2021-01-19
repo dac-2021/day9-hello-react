5055d27a6cbd40dcae7c993e4db5df60a286a451
koderoom <koderoom.dev@gmail.com>
2021-01-19 12:34:47 +0530

    working with props.

--- src/App.js
+++ src/App.js
@@ -3,55 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <Greeting1 />
-      <Greeting />
-      <Page1 />
+      <Page1 title="Delhi" id="1" />
+      <Page1 title="Kolkata" id="2" />
+      <Page1 title="Mumbai" id="3" />
     </div>
   );
 }
 
-function Page1() {
-  let list = ["Delhi", "Kolkata", "Mumbai", "Chennai"];
-
+function Page1(props) {
   return (
     <div>
-      <div>Working with List</div>
-
-      {list.map((item, index) => (
-        <h3 key={index}>{item}</h3>
-      ))}
-    </div>
-  );
-}
-
-function Greeting() {
-  const isMorning = true;
-
-  return (
-    <div>
-      <div>Message :</div>
-
-      {/* An Example of IF Condition */}
-      {isMorning && <div>Good Morning</div>}
-      {isMorning && (
-        <div>
-          <div>Multiline Good Morning</div>
-        </div>
-      )}
-    </div>
-  );
-}
-
-function Greeting1() {
-  const checkIfTrue = false;
-
-  return (
-    <div>
-      <div>IF and ELSE using Ternary Operator :</div>
-
-      {/* An Example of IF Condition */}
-      {/* expression ? 'Good Moring' : 'Good Afternoon' */}
-      {checkIfTrue ? <div>Good Morning</div> : <div>Good Afternoon</div>}
+      Hello World {props.id} {props.title}
     </div>
   );
 }
