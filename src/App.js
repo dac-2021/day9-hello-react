08a682b6f89705aa10766cf9c934881d6437e9e7
koderoom <koderoom.dev@gmail.com>
2021-01-19 14:01:46 +0530

    update.

--- src/App.js
+++ src/App.js
@@ -13,17 +13,9 @@ function Page1() {
   const [counter, setCounter] = useState(0);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
+    <div>
       <h1>{counter}</h1>
-      <button onClick={(e) => setCounter(counter + 1)}>Counter</button>
+      <button onClick={() => setCounter(counter + 1)}>Counter</button>
     </div>
   );
 }
