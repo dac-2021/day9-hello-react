c2d6ba2dfdb2efd918271398b2b8e6ff167c0b0d
koderoom <koderoom.dev@gmail.com>
2021-01-19 10:22:03 +0530

    master branch updated.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+/**
+ * Custom Tag
+ * Component <App />
+ */
 function App() {
   return (
     <div className="App">
